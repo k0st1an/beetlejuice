3e0b79cb8150f620a4ec206772f781d45325e5c5
Konstantin Kruglov <kruglovk@gmail.com>
2017-04-25 01:12:28 +0300

    refactoring sender

--- apps/sender/serializers.py
+++ apps/sender/serializers.py
@@ -5,9 +5,10 @@ from rest_framework import serializers
 
 class EmailSenderSerializer(serializers.Serializer):
     subject = serializers.CharField()
-    to = serializers.ListField()
+    to = serializers.ListField(child=serializers.CharField())
     text = serializers.CharField(required=False, allow_blank=True)
     html = serializers.CharField()
+    sender = serializers.CharField(required=False, allow_blank=True)
 
 
 class BodyField(serializers.Serializer):
--- apps/sender/views.py
+++ apps/sender/views.py
@@ -3,22 +3,23 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from backend.lib.sender import External
+from backend.lib.sender import Sender
 from backend.lib.decarators import require_token
 from apps.sender.serializers import (
     EmailSenderSerializer, DeliveryToInternalSerializer
 )
 
 
-class DeliveryToExternalView(APIView):
-    @require_token
+class BaseView(APIView):
+    serializer_class = None
+
     def post(self, request):
-        email = EmailSenderSerializer(data=request.data)
+        email = self.serializer_class(data=request.data)
 
         if not email.is_valid():
             return Response(email.errors, status=400)
 
-        send_ext = External(email.validated_data)
+        send_ext = Sender(email.validated_data)
 
         if not send_ext.send():
             Response({'status': False}, status=400)
@@ -26,12 +27,13 @@ class DeliveryToExternalView(APIView):
         return Response({'status': True}, status=200)
 
 
-class DeliveryToInternalView(APIView):
-    @staticmethod
-    def post(request):
-        email = DeliveryToInternalSerializer(data=request.data)
+class DeliveryToInternalView(BaseView):
+    serializer_class = DeliveryToInternalSerializer
 
-        if not email.is_valid():
-            return Response(email.errors, status=400)
 
-        return Response({'status': True}, status=200)
+class DeliveryToExternalView(BaseView):
+    serializer_class = EmailSenderSerializer
+
+    @require_token
+    def post(self, request):
+        return super(DeliveryToExternalView, self).post(request)
